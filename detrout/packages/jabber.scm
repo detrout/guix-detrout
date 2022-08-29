c8b5fb8725cfdcc6b0ecad820511c64bb246b368
Diane Trout <diane@ghic.org>
2022-08-29 12:34:35 -0700

    Looks like the python-2 package name was renamed to python2

--- detrout/packages/jabber.scm
+++ detrout/packages/jabber.scm
@@ -97,7 +97,7 @@ d sending commands.")
     (build-system gnu-build-system)
     (native-inputs
      `(("scons" ,scons)
-       ("python" ,python-2)))
+       ("python" ,python2)))
     (inputs
      `(("boost" ,boost)
        ("expat" ,expat)
