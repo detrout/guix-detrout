d3ba517526394a9329c9d98377416bf1bd0e90ee
Diane Trout <diane@ghic.org>
2022-09-09 23:41:39 -0700

    Don't seem to need to use this guix utils module

--- detrout/packages/pydata.scm
+++ detrout/packages/pydata.scm
@@ -41,7 +41,6 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module (guix utils)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system python)
