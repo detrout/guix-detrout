77a252eeb37324b571ed5b6c3b86b40184c6c145
Diane Trout <diane@ghic.org>
2022-09-10 09:54:05 -0700

    Add rust-ntex-h2-0.1

--- detrout/packages/crates-io.scm
+++ detrout/packages/crates-io.scm
@@ -1500,6 +1500,48 @@ inter-operation between primitives and enums easier)")
     (description "glommio intergration for ntex framework")
     (license license:expat)))
 
+(define-public rust-ntex-h2-0.1
+  (package
+    (name "rust-ntex-h2")
+    (version "0.1.4")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "ntex-h2" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "10r9kfzgxa4nyb7lbz8h5280hpk9l6fmqa54kpra6xcdfc9zlj5v"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-bitflags" ,rust-bitflags-1)
+                       ("rust-fxhash" ,rust-fxhash-0.2)
+                       ("rust-log" ,rust-log-0.4)
+                       ("rust-ntex-bytes" ,rust-ntex-bytes-0.1)
+                       ("rust-ntex-codec" ,rust-ntex-codec-0.6)
+                       ("rust-ntex-connect" ,rust-ntex-connect-0.1)
+                       ("rust-ntex-http" ,rust-ntex-http-0.1)
+                       ("rust-ntex-io" ,rust-ntex-io-0.1)
+                       ("rust-ntex-rt" ,rust-ntex-rt-0.4)
+                       ("rust-ntex-service" ,rust-ntex-service-0.3)
+                       ("rust-ntex-util" ,rust-ntex-util-0.1)
+                       ("rust-pin-project-lite" ,rust-pin-project-lite-0.2)
+                       ("rust-thiserror" ,rust-thiserror-1))
+       #:cargo-development-inputs (("rust-env-logger" ,rust-env-logger-0.9)
+                                   ("rust-hex" ,rust-hex-0.4)
+                                   ("rust-ntex" ,rust-ntex-0.5)
+                                   ("rust-ntex-connect" ,rust-ntex-connect-0.1)
+                                   ("rust-ntex-tls" ,rust-ntex-tls-0.1)
+                                   ("rust-openssl" ,rust-openssl-0.10)
+                                   ("rust-quickcheck" ,rust-quickcheck-1)
+                                   ("rust-rand" ,rust-rand-0.8)
+                                   ("rust-serde" ,rust-serde-1)
+                                   ("rust-serde-json" ,rust-serde-json-1)
+                                   ("rust-walkdir" ,rust-walkdir-2))))
+    (home-page "https://github.com/ntex-rs/ntex-h2")
+    (synopsis "An HTTP/2 client and server")
+    (description "An HTTP/2 client and server")
+    (license license:expat)))
+
 (define-public rust-ntex-io-0.1
   (package
     (name "rust-ntex-io")
