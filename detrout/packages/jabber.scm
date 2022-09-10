29cd1a5e10726db04c9efa08603239db9cfabb3f
Diane Trout <diane@ghic.org>
2022-09-10 11:22:36 -0700

    scons-python2 definition moved

--- detrout/packages/jabber.scm
+++ detrout/packages/jabber.scm
@@ -9,6 +9,7 @@
   #:use-module (gnu packages apr)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages boost)
+  #:use-module (gnu packages build-tools)
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages curl)
